e8949994f1531226997eb4746f3b6bedadb4f50a
SalahMSwefy <salahm.swefy@gmali.com>
2025-04-23 18:55:21 +0200

    new changes

--- Front-end/src/pages/Users.jsx
+++ Front-end/src/pages/Users.jsx
@@ -1,5 +1,16 @@
+import UsersTable from "../features/users/UsersTable";
+
 function Users() {
-    return <div>Users</div>;
+    return (
+        <div className="flex h-full flex-col gap-4 bg-primary-50 p-4">
+            <h1 className="text-2xl font-bold capitalize text-primary-900">
+                Users tables
+            </h1>
+            <div className="flex-1">
+                <UsersTable />
+            </div>
+        </div>
+    );
 }
 
 export default Users;
--- Front-end/src/ui/Plate.jsx
+++ Front-end/src/ui/Plate.jsx
@@ -1,13 +1,12 @@
 function Plate() {
     return (
-        <div className="flex h-[80px] w-[165px] flex-col border border-black text-lg">
-            {/* Top blue section */}
-            <div className="flex items-center justify-between bg-primary-700 px-2 font-bold text-white">
+        <div className="flex h-20 w-36 flex-col border border-black text-lg">
+            <div className="flex items-center justify-between bg-primary-700 px-1 font-bold text-white">
                 <span>EGYPT</span>
                 <span className="font-arabic">مصر</span>
             </div>
 
-            <div className="grid flex-1 grid-cols-2 content-center text-center font-arabic text-2xl font-bold text-black">
+            <div className="grid grow grid-cols-2 font-arabic text-xl font-bold text-black">
                 <div className="flex items-center justify-center border-r border-black">
                     ٣ ٩ ٤ ٥
                 </div>
