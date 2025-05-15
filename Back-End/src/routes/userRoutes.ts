a0034286fe1bb9bb96615ef48cb09541d1d05039
Bor3y9 <yousef161247@eng.psu.edu.eg>
2025-05-15 16:27:03 +0300

    Implemente Updating the user throw an admin(Owner role only)

--- Back-End/src/routes/userRoutes.ts
+++ Back-End/src/routes/userRoutes.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 import userController from '../controllers/userController';
+import authController from '../controllers/authController';
 
 const router = express.Router({ mergeParams: true });
 
+router.use(authController.protect);
+router.use(authController.restrictTo('Owner','Observer'));
+
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUser);
+
+router.use(authController.restrictTo('Owner'));
+
+router.patch('/:id', userController.editUser);
+
 
 export default router;
