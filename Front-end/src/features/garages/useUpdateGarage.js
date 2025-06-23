2c38b10ae2699c6372507148a56a49e9cc22f46e
SalahMSwefy <salahm.swefy@gmali.com>
2025-06-23 20:35:41 +0300

    some fix

--- Front-end/src/features/garages/useUpdateGarage.js
+++ Front-end/src/features/garages/useUpdateGarage.js
@@ -1,20 +1,26 @@
-import { toast } from "react-hot-toast";
+import toast from "react-hot-toast";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateGarage as updateGarageApi } from "../../services/apiGarages";
 
 export function useUpdateGarage() {
     const queryClient = useQueryClient();
 
-    const { mutate: updateGarage, isPending } = useMutation({
-        mutationFn: (data) => updateGarageApi(data),
+    const { mutate: updateGarage, error } = useMutation({
+        mutationKey: ["updateGarage"],
+        mutationFn: (data) =>
+            toast.promise(updateGarageApi(data), {
+                loading: "Updating garage...",
+                success: (data) =>
+                    data.message || "Garage updated successfully!",
+                error: (err) => err?.message || "Failed to update garage.",
+            }),
         onSuccess: () => {
-            toast.success("Garage information updated successfully");
             queryClient.invalidateQueries({ queryKey: ["admin"] });
         },
-        onError: (err) => {
-            toast.error(err.message || "Failed to update garage information");
+        onError: (error) => {
+            console.error("Error updating garage:", error);
         },
     });
 
-    return { updateGarage, isPending };
+    return { updateGarage, error };
 }
